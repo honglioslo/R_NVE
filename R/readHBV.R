98136cdbbd133d7845dfc0814d7c34e5c8c76ff0
Hong Li (Rød Plomme) <honglioslo@users.noreply.github.com>
2017-10-09 10:40:13 +0200

    remove test
    remove test

--- R/readHBV.R
+++ R/readHBV.R
@@ -1,8 +1,17 @@
-readHBV <- function(fileName) {
+#' read nve data
+#'
+#' This function read the HBV format data.
+#' @fileName file to read.
+#' @keywords data
+#' @export
+#' @examples
+#' readHBV()
+
+readHBV <- function(fileName = "") {
 	print(fileName)
 	ObsRunoff <- read.table(fileName, header = FALSE)
 	ObsRunoff[which(ObsRunoff[,2] == -9999),2] <- NA
 	ObsDates <- as.Date(ObsRunoff[,1], format = "%Y%m%d/1200")
 	ObsRunoffZoo <- zoo(ObsRunoff[,2], ObsDates)
-
+	return(ObsRunoffZoo)
 }
