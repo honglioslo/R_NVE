5a9097aea898b15ee282e2653a51c639d173e855
Hong Li (Rød Plomme) <honglioslo@users.noreply.github.com>
2017-10-09 11:05:56 +0200

    Add files via upload

--- R/readHBV.R
+++ R/readHBV.R
@@ -7,11 +7,11 @@
 #' @examples
 #' readHBV()
 
-readHBV <- function(fileName = "") {
+readHBV <- function(fileName = "data/dew_00400000.out") {
+	if ( ! require(zoo) )        { install.packages("zoo");        library(zoo) }
 	print(fileName)
 	ObsRunoff <- read.table(fileName, header = FALSE)
 	ObsRunoff[which(ObsRunoff[,2] == -9999),2] <- NA
 	ObsDates <- as.Date(ObsRunoff[,1], format = "%Y%m%d/1200")
 	ObsRunoffZoo <- zoo(ObsRunoff[,2], ObsDates)
-	return(ObsRunoffZoo)
 }
